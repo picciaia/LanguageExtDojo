69c4ebe0084014bd5b6f03ec114398202b007d04
picciaia <picciaia@gmail.com>
2023-04-06 09:31:13 +0200

    Initial commit

---
+++ LanguageExtDojo/BookSamples/Chapter01.cs
@@ -0,0 +1,73 @@
+﻿using static System.Console;
+using static Factories;
+
+class Chapter01
+{
+    public void Run()
+    {
+        (Func<int> zero, Func<int, int, int> add) monoid = (zero: () => 0, add: (x, y) => x + y);
+        monoid.zero();
+
+        Func<(Order, double discount), (Order updatedOrder, double totalPrice)> updateOrder = state => { return (new Order(1), 0.1); };
+        Func<(Order, double totalPrice), (double totalPrice, string errors)> validate = state => { return (0.1, "noerror"); };
+
+        Func<(Order, double discount), (double totalPrice, string errors)> proccess =
+            order => validate(updateOrder(order));
+
+        var ret = (1, 2).Map(x => x * x);
+
+        WriteLine(ret);
+        var t = IO(() => 1);
+
+        string F(int x, int y) => (x + y).ToString();
+
+        Func<int, string> F1(int x)
+        {
+            string g(int y) { return (x + y).ToString(); }
+            return g;
+        }
+
+        var s = F(3, 4);
+        var s1 = F1(3)(4);
+
+        Func<int, Func<int, string>> F1b = x => y => { return (x + y).ToString(); };
+        var s1b = F1b(3)(4);
+
+        Func<double, double, double> discountedPrice = (discount, price) =>
+            price - discount * price;
+
+        Func<double, Func<double, double>> discountedPriceCurried = discount => price =>
+            price - discount * price;
+
+        var discountedPrices = new List<double> { 10, 20, 30 }.Select(x => discountedPrice(0.1, x));
+        var discountedPrices1 = new List<double> { 10, 20, 30 }.Select(discountedPriceCurried(0.1));
+
+        Func<double, double> discountedPricePartial = x => discountedPrice(0.1, x);
+
+        static Func<T, Func<T, T>> Curry<T> (Func<T, T, T> f) => x => y => f(x, y);
+
+        var c = Curry(discountedPrice)(0.1);
+    }
+}
+
+
+class IO<T>
+{
+    public Func<T> Fn { get; set; }
+    public IO(Func<T> fn) => Fn = fn;
+    public T Run() => Fn();
+}
+
+class Factories
+{
+    public static IO<T> IO<T>(Func<T> fn) => new IO<T>(fn);
+}
+
+static class FunctionalExt
+{
+    public static ValueTuple<T1, T1> Map<T, T1>(this ValueTuple<T, T> @this, Func<T, T1> f) =>
+        new ValueTuple<T1, T1>(f(@this.Item1), f(@this.Item2));
+}
+
+
+
---
+++ LanguageExtDojo/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using LanguageExt;
+
+
+var persons = new [] { new Person("Jon", 1), new Person("Peter", 2) }.ToList();
+var animals = new[] { new Animal("Cat", 1), new Animal("Dog", 2) }.ToList();
+
+Either<Person, Animal> p = persons[0];
+Either<Person, Animal> a = animals[0];
+IEnumerable<Either<Person, Animal>> lst = new[] { p, a };
+
+var allNames = persons.Bind(x => x.Name);
+var allNames2 = persons.SelectMany(x=>x.Name);
+
+var allNames3 = persons.Map(x => x.Name);
+
+
+Console.WriteLine("bye");
+
+string ToUpper(string s) => s.ToUpper();
+int ChangeID(int ID) => ID++;
+
+record struct Person(string Name, int ID) { }
+record struct Animal(string Name, int ID) { }
+record struct Order(int ID) { }
+
